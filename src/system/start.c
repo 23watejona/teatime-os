1220979b1190f787b8a4d59921fa0359769a1648
kali <kali@jonathan-cb>
2025-12-25 23:40:49 -0500

    Create _set_vec_base

--- src/system/start.c
+++ src/system/start.c
@@ -1,29 +1,16 @@
 #include "uart.h"
+#include "reg_util.h"
 
-int s = 0;
-void notmain ( void )
+void start ( void )
 {
-    __asm__ __volatile__(
-        "movi       a0, 0x40100000\n"
-        "wsr        a0, vecbase\n"
-        : : :"memory");
+    _set_vec_base();
+    BUSY_WAIT();
+    initmem();
+    alloc_stack(512);
+
     while(1)
     {
-        for(int i=0;i<10000000;i++);
-        kprintf_uart("\nTesting %d\n", 100);
-        kprintf_uart("About to break narrow\n");
-        asm("break.n 0");
-        kprintf_uart("About to break regular\n");
-        asm("break 0,0");
-        kprintf_uart("Successfully here\n");
-        asm("syscall");
-        kprintf_uart("just syscalled");
+        BUSY_WAIT();
+        kprintf_uart("Running main\n");
     }
-}
-
-void print_stuff() {
-  if (s == 0) {
-    kprintf_uart("testing testing testing\n");
-    s = 1;
-  }
 }
