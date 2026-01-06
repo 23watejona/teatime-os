4803f4a9076baeafec054ae54d5cdde518171da0
kali <kali@jonathan-cb>
2026-01-06 04:38:27 -0500

    Simplify and clarify uart code

--- src/device/uart/uart.c
+++ src/device/uart/uart.c
@@ -1,78 +1,82 @@
 #include "stdarg.h"
-
-#define READ_PERI_REG(addr)  (*((volatile unsigned int *)addr))
-#define WRITE_PERI_REG(addr, val) (*((volatile unsigned int *)addr)) = (unsigned int)(val)
-
-#define UART_STATUS 0x6000001C
-#define UART_AUTOBAUD 0x60000018
-#define UART_FIFO 0x60000000
-#define UART_CONF0 0x60000020
-#define UART_CLKDIV 0x60000014
-#define UART_INT_CLR 0x60000010
-#define UART_TXFIFO_CNT 0x000000FF
-#define UART_TXFIFO_CNT_S 16
+#include "uart.h"
 
 char *itoau(int, char *, int);
 char *itoa(int, char *, int);
 
+inline __attribute__((always_inline)) unsigned int uart0_tx_fifo_free() {
+    return TX_FIFO_SIZE - uart0.status.tx_fifo_count;
+}
+
+inline __attribute__((always_inline)) unsigned int uart0_tx_fifo_size() {
+    return uart0.status.tx_fifo_count;
+}
+
+inline __attribute__((always_inline)) unsigned int uart0_tx_fifo_full() {
+    return uart0_tx_fifo_size() >= TX_FIFO_SIZE;
+}
+
+void uart0_flush() {
+    while (uart0_tx_fifo_size());
+}
+
 void kputc_uart(int c) {
- if (c == '\n') {
-    kputc_uart('\r');
-  }  
-  while (1) {
-        unsigned int fifo_cnt = READ_PERI_REG(UART_STATUS) & (UART_TXFIFO_CNT << UART_TXFIFO_CNT_S);
-
-        if ((fifo_cnt >> UART_TXFIFO_CNT_S & UART_TXFIFO_CNT) < 126)
-            break;
+    // add '\r' before a newline
+    if (c == '\n') {
+        // block until we can put a char
+        while(uart0_tx_fifo_full());
+        uart0.fifo.rw = '\r';
     }
-
-    WRITE_PERI_REG(UART_FIFO, c);
+    // block until we can put a char
+    while(uart0_tx_fifo_full());
+    uart0.fifo.rw = c;
 }
 
 void kprintf_uart(char *f, ...) {
-  va_list args;
-  va_start(args, f);
-  while ((unsigned int)*f != (unsigned int)'\0') {
-    if (*f != '%') {
-      kputc_uart(*f++);
-    } else {
-      switch (*(++f)) {
-       case 'd':
-          {
-            int tmp_d = va_arg(args, int);
-            char buf[12] = {0};
-            kprintf_uart(itoa(tmp_d, buf, 10));
-            ++f;
-          }
-          break;
-       case 'u':
-          {
-            unsigned int tmp_d = va_arg(args, unsigned int);
-            char buf[12] = {0};
-            kprintf_uart(itoau(tmp_d, buf, 10));
-            ++f;
-          }
-          break;
-       case 'x':
-          {
-            unsigned int tmp_d = va_arg(args, unsigned int);
-            char buf[12] = {0};
-	    kprintf_uart("0x");
-            kprintf_uart(itoau(tmp_d, buf, 16));
-            ++f;
-          }
-          break;
-	case 's':
-	  {
-	    char *str = va_arg(args, char *);
-	    kprintf_uart(str);
-	    ++f;
-	  }
-	  break;
-        default:
-          kputc_uart(*f++);
-      }
+    va_list args;
+    va_start(args, f);
+    while ((unsigned int)*f != (unsigned int)'\0') {
+        if (*f != '%') {
+            kputc_uart(*f++);
+        } else {
+            switch (*(++f)) {
+                case 'd':
+                    {
+                        int tmp_d = va_arg(args, int);
+                        char buf[12] = {0};
+                        kprintf_uart(itoa(tmp_d, buf, 10));
+                        ++f;
+                    }
+                    break;
+                case 'u':
+                    {
+                        unsigned int tmp_d = va_arg(args, unsigned int);
+                        char buf[12] = {0};
+                        kprintf_uart(itoau(tmp_d, buf, 10));
+                        ++f;
+                    }
+                    break;
+                case 'x':
+                    {
+                        unsigned int tmp_d = va_arg(args, unsigned int);
+                        char buf[12] = {0};
+                        kprintf_uart("0x");
+                        kprintf_uart(itoau(tmp_d, buf, 16));
+                        ++f;
+                    }
+                    break;
+                case 's':
+                    {
+                        char *str = va_arg(args, char *);
+                        kprintf_uart(str);
+                        ++f;
+                    }
+                    break;
+                default:
+                    kputc_uart(*f++);
+            }
+        }
     }
-  }
+    uart0_flush();
 }
 
--- src/include/uart.h
+++ src/include/uart.h
@@ -4,6 +4,122 @@
 void kprintf_uart(char *f, ...);
 void kputc_uart(int c);
 
+struct uart_ctrl {
+    struct {
+        char rw;
+        char reserved[3]; // pad to 4 bytes
+    } fifo;
+    struct {
+        char rxfifo_full: 1;
+        char txfifo_empty: 1;
+        char parity_err: 1;
+        char frm_err: 1;
+        char rxfifo_overflow: 1;
+        char dsr_change: 1;
+        char cts_change: 1;
+        char brk_detected: 1;
+        char rxfifo_timeout: 1;
+        unsigned int reserved: 23;
+    } int_raw;
+    struct {
+        char rxfifo_full: 1;
+        char txfifo_empty: 1;
+        char parity_err: 1;
+        char frm_err: 1;
+        char rxfifo_overflow: 1;
+        char dsr_change: 1;
+        char cts_change: 1;
+        char brk_detected: 1;
+        char rxfifo_timeout: 1;
+        unsigned int reserved: 23;
+    } int_status;
+
+    struct {
+        char rxfifo_full: 1;
+        char txfifo_empty: 1;
+        char parity_err: 1;
+        char frm_err: 1;
+        char rxfifo_overflow: 1;
+        char dsr_change: 1;
+        char cts_change: 1;
+        char brk_detected: 1;
+        char rxfifo_timeout: 1;
+        unsigned int reserved: 23;
+    } int_enable; // enable/disable interrupts
+    
+    struct {
+        char rxfifo_full: 1;
+        char txfifo_empty: 1;
+        char parity_err: 1;
+        char frm_err: 1;
+        char rxfifo_overflow: 1;
+        char dsr_change: 1;
+        char cts_change: 1;
+        char brk_detected: 1;
+        char rxfifo_timeout: 1;
+        unsigned int reserved: 23;
+    } int_clear; // clear interrupt status
+
+    struct {
+        unsigned int div_int: 20;
+        unsigned int reserved: 12;
+    } clk_div; // set clock div
+
+    struct {
+        unsigned int enable: 1;
+        unsigned int reserved1: 7;
+        unsigned int glitch_filter: 8;
+        unsigned int reserved2: 16;
+    } auto_baud;
+
+    struct {
+        unsigned int rx_fifo_count: 8;
+        unsigned int reserved1: 5;
+        unsigned int dsrn: 1;
+        unsigned int ctsn: 1;
+        unsigned int rxd: 1;
+        unsigned int tx_fifo_count: 8;
+        unsigned int reserved2: 5;
+        unsigned int dtrn: 1;
+        unsigned int rtsn: 1;
+        unsigned int txd: 1;
+    } status;
+
+    struct {
+        unsigned int x;
+
+    } conf0;
+
+    struct {
+        unsigned int x;
+
+    } conf1;
+
+    struct {
+        unsigned int min_duration: 20;
+        unsigned int reserved: 12;
+
+    } low_pulse;
+
+    struct {
+        unsigned int min_duration: 20;
+        unsigned int reserved: 12;
+    } high_pulse;
+
+    struct {
+        unsigned int count: 10;
+        unsigned int reserved: 22;
+
+    } rxd_count;
+
+    unsigned int reserved[17];
+    unsigned int date;
+    unsigned int id;
+};
+
+extern struct uart_ctrl uart0;
+
+#define TX_FIFO_SIZE (127) // it's actually 128 but we give it a byte of wiggle
+#define RX_FIFO_SIZE (127) // it's actually 128 but we give it a byte of wiggle
+
 #endif
-
-
