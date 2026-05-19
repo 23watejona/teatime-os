ecf80cba244508e7d9ca01ca89b27ad71d0d4b23
Jonathan Waters <jonathan@waters.to>
2026-05-18 22:39:33 -0400

    kernel: map RTC peripheral block + linker symbol

--- src/include/timer.h
+++ src/include/timer.h
@@ -31,4 +31,90 @@ extern volatile struct frc1_t frc1;
 
 _Static_assert(sizeof(struct frc1_t) == 16, "sizeof(struct frc1_t) != 16");
 
+struct rtc_t {
+    unsigned int _res00;
+
+    unsigned int slp_val;
+
+    unsigned int analog_0;
+
+    unsigned int pwr;
+
+    unsigned int _res10;
+
+    struct {
+        unsigned int cause: 4;
+        unsigned int reserved: 28;
+    } state1;
+
+    struct {
+        unsigned int reserved_lo: 8;
+        unsigned int wakeup_cause: 6;
+        unsigned int reserved_hi: 18;
+    } state2;
+
+    unsigned int slp_cnt_val;
+
+    unsigned int sleep_state;
+
+    unsigned int sleep_mask;
+
+    unsigned int status;
+
+    unsigned int _res2c;
+
+    unsigned int scratch[4];
+
+    unsigned int _res40[2];
+
+    unsigned int timing[2];
+
+    unsigned int _res50[2];
+
+    unsigned int analog_1;
+
+    unsigned int analog_2;
+
+    unsigned int analog_3;
+
+    unsigned int _res64;
+
+    unsigned int gpio_out;
+
+    unsigned int _res6c[2];
+
+    unsigned int gpio_enable;
+
+    unsigned int _res78[2];
+
+    unsigned int analog_6;
+
+    unsigned int _res84[2];
+
+    struct {
+        unsigned int level: 1;
+        unsigned int reserved: 31;
+    } gpio_in_data;
+
+    unsigned int gpio_conf;
+
+    unsigned int clk_1;
+
+    unsigned int clk_2;
+
+    unsigned int clk_3;
+
+    unsigned int pad_xpd_dcdc_conf;
+
+    unsigned int _resa4[2];
+
+    unsigned int trim[3];
+
+    unsigned int _resb8[18];
+};
+
+extern volatile struct rtc_t rtc;
+
+_Static_assert(sizeof(struct rtc_t) == 256, "sizeof(struct rtc_t) != 256");
+
 #endif // TIMER_H
